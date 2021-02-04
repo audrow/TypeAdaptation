0755eb2cfec4f15d0bf85969bfe52a501adad4f1
Audrow Nash <audrow@hey.com>
2021-02-04 23:03:11 +0000

    After talking with William

--- rclcpp/rclcpp.hpp
+++ rclcpp/rclcpp.hpp
@@ -26,6 +26,21 @@ void TypeAdapter<std::string, std_msgs::msg::String>::serialize(std::string data
   std::cout << "Serialize std::string '" << data << "'\n";
 }
 
+template<class CustomType, class ROSInterfaceType>
+struct ROSMessage
+{
+  using ROSInterfaceType = ROSInterfaceType;
+  using ActualType = CustomType;
+
+  static
+  const rosidl_typesupport_t *
+  get_typesupport()
+  {
+    // from:
+    //  https://github.com/ros2/rclcpp/blob/1c92e6d6091aae88422ad568761b736abf846e98/rclcpp/include/rclcpp/publisher.hpp#L81
+    return rosidl_typesupport_cpp::get_message_type_support_handle<ROSInterfaceType>();
+  }
+};
 
 template <typename T>
 void send(T msg);
@@ -35,8 +50,7 @@ void send(std_msgs::msg::String msg) {
   std::cout << "Sending std_msgs::msg::String '" << msg.data << "'\n";
 }
 
-
-template <typename AdapterType>
+template <typename ROSMessageT>
 struct Publisher {
   std::string name;
   int queue_size;
@@ -44,10 +58,15 @@ struct Publisher {
   Publisher(std::string name_, int queue_size_) {
     name = name_;
     queue_size = queue_size_;
+    rcl_publisher_t rcl_publisher = rcl_get_zero_initialized_publisher();
+    rcl_ret_t ret = rcl_publisher_init(
+      &rcl_publisher,
+      ROSMessageT::get_typesupport(),
+      /* ... */
+    );
   }
 
-  template <typename CustomType, typename ROSInterfaceType>
-  void publish(CustomType content) {
+  void publish(const typename ROSMessageT::ActualType & content) {
     // TODO figure out how to pass adapter in but keep this pattern
     ROSInterfaceType msg;
     AdapterType::serialize(content, msg);
@@ -55,6 +74,9 @@ struct Publisher {
   }
 };
 
+template<class NodeT, class MessageT>
+std::shared_ptr<rclcpp::Publisher<MessageT>>
+rclcpp::create_publisher<MessageT>(NodeT, "topic name", 10);
 
 struct Node {
   std::string name;
@@ -66,6 +88,7 @@ struct Node {
   }
   template <typename AdapterType>
   std::shared_ptr<Publisher<AdapterType>> create_publisher(std::string name, int queue_size) {
+    // TODO(wjwwood): should be rclcpp::create_publisher()
     return std::make_shared<Publisher<AdapterType>>(name, queue_size);
   }
 };
